2451d2ac9cd16c13cb9ccf0bf067864cbe92ee5e
allan <allanrey.macasaquit@gmail.com>
2023-11-13 20:06:19 -0500

    Lesson 1. Connect Mongoose database.

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 dotenv.config();
 
 const app = express();
+
+// Connect to MongoDB
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		console.log('Connected to MongoDB');
+	})
+	.catch((error) => {
+		console.error('Error connecting to MongoDB:', error);
+	});
 
 const port = process.env.Port || 4000;
 
