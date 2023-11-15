8adba7309790b3127053be0d6400e5677e244611
allan <allanrey.macasaquit@gmail.com>
2023-11-14 22:37:20 -0500

    Lesson 6. Completed User Crud Functionality

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -71,3 +71,117 @@ export const signinUser = async (req, res, next) => {
 		return res.status(550).json({ message: 'Internal Server Error' });
 	}
 };
+
+export const getUsers = async (req, res, next) => {
+	try {
+		const getUsers = await User.find();
+		//Mapping through users to remove the password field before sending the response
+		const usersWithoutPasswords = getUsers.map((users) => {
+			const { password, ...userWithoutPassword } = users.toObject(); // Exclude the password field
+			return userWithoutPassword;
+		});
+
+		res.json({
+			message: 'Successfully',
+			users: usersWithoutPasswords,
+		});
+	} catch (error) {
+		console.error(error);
+		next(error);
+	}
+};
+
+export const getUserById = async (req, res, next) => {
+	const userId = req.params.id; // Extracting user ID from request parameters
+
+	try {
+		const user = await User.findById(userId);
+
+		if (!user) {
+			return res.status(404).json({ message: 'User not found' });
+		}
+
+		// Exclude the password field from the user object before sending the response
+		const { password, ...userWithoutPassword } = user.toObject();
+
+		res.json({
+			message: 'Successfully',
+			user: userWithoutPassword,
+		});
+	} catch (error) {
+		console.error(error);
+		next(errorHandler('401', 'Unauthorized'));
+	}
+};
+
+export const deleteUser = async (req, res, next) => {
+	const userId = req.params.id; // Extracting user ID from request parameters
+
+	try {
+		const deletedUser = await User.findByIdAndDelete(userId);
+
+		if (!deletedUser) {
+			return res.status(404).json({ message: 'User not found' });
+		}
+
+		// Exclude the password field from the deleted user object before sending the response
+		const { password, ...deletedUserWithoutPassword } = deletedUser.toObject();
+
+		res.json({
+			message: 'User deleted successfully',
+			deletedUser: deletedUserWithoutPassword,
+		});
+	} catch (error) {
+		console.error(error);
+		next(errorHandler('401', 'Unauthorized'));
+	}
+};
+
+export const updateUser = async (req, res, next) => {
+	const userId = req.params.id; // Extracting user ID from request parameters
+	const { email, mobile, password, firstname, lastname } = req.body; // Data to update, received from request body
+
+	try {
+		const userToUpdate = await User.findById(userId);
+
+		if (!userToUpdate) {
+			return res.status(404).json({ message: 'User not found' });
+		}
+
+		// Update allowed fields if they exist in the request
+		if (firstname) {
+			userToUpdate.firstname = firstname;
+		}
+
+		if (lastname) {
+			userToUpdate.lastname = lastname;
+		}
+		if (email) {
+			userToUpdate.email = email;
+		}
+
+		if (mobile) {
+			userToUpdate.mobile = mobile;
+		}
+
+		if (password) {
+			// If updating password, hash the new password
+			userToUpdate.password = await bcryptjs.hash(password, 10);
+		}
+
+		// Save the updated user
+		const updatedUser = await userToUpdate.save();
+
+		// Exclude sensitive fields from the updated user object before sending the response
+		const { password: updatedPassword, ...updatedUserWithoutPassword } =
+			updatedUser.toObject();
+
+		res.json({
+			message: 'User updated successfully',
+			updatedUser: updatedUserWithoutPassword,
+		});
+	} catch (error) {
+		console.error(error);
+		next(errorHandler('401', 'Unauthorized'));
+	}
+};
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,10 +1,20 @@
 import express from 'express';
-import { createUser, signinUser } from '../controllers/UserController.js';
+import {
+	createUser,
+	deleteUser,
+	getUserById,
+	getUsers,
+	signinUser,
+	updateUser,
+} from '../controllers/UserController.js';
 
 const userRoute = express.Router();
 
 userRoute.post('/register', createUser);
-
 userRoute.post('/signin', signinUser);
+userRoute.get('/get_users', getUsers);
+userRoute.get('/get_user/:id', getUserById);
+userRoute.delete('/delete_user/:id', deleteUser);
+userRoute.post('/update_user/:id', updateUser);
 
 export default userRoute;
